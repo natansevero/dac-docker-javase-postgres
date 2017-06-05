d7165ea9403c31fa740b16656b3f61a1f40bca7e
natansevero <natanseverolp@gmail.com>
2017-06-05 15:52:32 -0300

    Adicionando arquivo docker-compose

--- postgres/tabela.sql
+++ postgres/tabela.sql
@@ -5,3 +5,5 @@ create table produto(
 	tipo text,
 	primary key(id)
 );
+insert into produto (nome, preco, tipo) values ('arroz', 2.50, 'alimento');
+insert into produto (nome, preco, tipo) values ('pedra sanitaria', 3.00, 'limpeza');
